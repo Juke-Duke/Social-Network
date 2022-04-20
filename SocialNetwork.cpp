7e82ecf90a8b22b61c6e942440d83250f5ed1cde
JukeDuke <masterofegy@gmail.com>
2022-04-20 03:39:34 -0400

    clean

--- SocialNetwork.cpp
+++ SocialNetwork.cpp
@@ -2,165 +2,123 @@
 #include <iostream>
 #include <cctype>
 #include <string>
-using namespace std;
+using std::string;
 
 struct Post
 {
-    std::string username;
-    std::string message;
+    string username, message;
 };
 
 class Profile
 {
 private:
-    std::string username;
-    std::string displayname;
+    string username, displayname, fullDisplay;
+
 public:
-    // Profile constructor for a user (initializing 
-    // private variables username=usrn, displayname=dspn)
-    Profile(std::string usrn, std::string dspn) : username(usrn), displayname(dspn) {}
-    // Default Profile constructor (username="", displayname="")
-    Profile() : username(" "), displayname(" ") {}
-    // Return username
-    std::string getUsername()
-    {
-        return username;
-    }
-    // Return name in the format: "displayname (@username)"
-    std::string getFullName()
-    {
-        std::string fullDisplay = displayname + " (@" + username + ")";
-        return fullDisplay;
-    }
-    // Change display name
-    void setDisplayName(std::string dspn)
-    {
-        displayname = dspn;
-    }
+    Profile() : username(" "), displayname(" "), fullDisplay(" ") {}
+
+    Profile(const string& usrn, const string& dspn) : username(usrn), displayname(dspn) { fullDisplay = displayname + " (@" + username + ")"; }
+
+    string getUsername() const { return username; }
+
+    string getFullName() const { return fullDisplay; }
+   
+    void setDisplayName(const string& dspn) { displayname = dspn; }
 };
 
 class Network
 {
 private:
-    static const int MAX_USERS = 20; // max number of user profiles
-    int numUsers;                    // number of registered users
-    Profile profiles[MAX_USERS];     // user profiles array:
-                                     // mapping integer ID -> Profile
-    bool following[MAX_USERS][MAX_USERS];  // friendship matrix:
-                                           // following[id1][id2] == true when id1 is following id2
-    static const int MAX_POSTS = 100; // max number of posts
-    int numPosts;                    // number of posts
-    Post posts[MAX_POSTS];           // array of all posts
-    // Returns user ID (index in the 'profiles' array) by their username
-    // (or -1 if username is not found)
-    int findID(string usrn)
+    static const int MAX_USERS = 20, MAX_POSTS = 100;
+    int numUsers, numPosts;
+    Post posts[MAX_POSTS];
+    Profile profiles[MAX_USERS];
+    bool following[MAX_USERS][MAX_USERS];
+
+    int findID(const string& usrn) const
     {
-        for (int i = 0; i < MAX_USERS; i++)
-        {
+        for (int i = 0; i < numUsers; ++i)
             if (profiles[i].getUsername() == usrn)
                 return i;
-        }
 
         return -1;
     }
-public:
-    // Constructor, makes an empty network (numUsers = 0)
+
+public: 
     Network() : numUsers(0), numPosts(0)
     {
-        for (int row = 0; row < MAX_USERS; row++)
-            for (int col = 0; col < MAX_USERS; col++)
+        for (int row = 0; row < MAX_USERS; ++row)
+            for (int col = 0; col < MAX_USERS; ++col)
                 following[row][col] = false;
     }
-    // Attempts to sign up a new user with specified username and displayname
-    // return true if the operation was successful, otherwise return false
-    bool addUser(string usrn, string dspn)
-    {
-        // Checks to see if there is still place left for new users
-        if (numUsers < MAX_USERS)
-        {
-            // Makes sure all characters are alphanumerical
-            for (int i = 0; i < usrn.size(); i++)
-            {
-                if (!((usrn[i]) || isdigit(usrn[i])))
-                    return false;
-            }
 
-            // Checks to see if the username exists already
-            if (findID(usrn) != -1)
+    bool addUser(const string& usrn, const string& dspn)
+    {
+        if (numUsers == MAX_USERS || findID(usrn) != -1)
+            return false;
+
+        for (char letter : usrn)
+            if (!isalnum(letter))
                 return false;
 
-            Profile newUser(usrn, dspn);   // Creates a new user Profile
-            profiles[numUsers] = newUser;  // Adds user to the array of profiles
-            numUsers++;                    // Increments the number of users
-            return true;
-        }
+        profiles[numUsers] = Profile(usrn, dspn);
+        ++numUsers;
 
-        return false;
+        return true;
     }
 
-    // Make 'usrn1' follow 'usrn2' (if both usernames are in the network).
-    // return true if success (if both usernames exist), otherwise return false
-    bool follow(string usrn1, string usrn2)
+    bool follow(const string& usrn1, const string& usrn2)
     {
-        // Makes sure both users exist
-        if (findID(usrn1) != -1 && findID(usrn2) != -1)
-        {
-            following[findID(usrn1)][findID(usrn2)] = true;
-            return true;
-        }
+        int id1 = findID(usrn1), id2 = findID(usrn2);
 
-        return false;
+        if (id1 == -1 || id2 == -1)
+            return false;
+
+        following[id1][id2] = true;
+
+        return true;
     }
 
-    // Print Dot file (graphical representation of the network)
-    void printDot()
+    void printDot() const
     {
-        std::cout << "digraph {" << std::endl;
-        for (int i = 0; i < numUsers; i++)
-            std::cout << "\t\"@" << profiles[i].getUsername() << "\"""" << std::endl;
+        std::cout << "digraph \n{" << std::endl;
+        for (int i = 0; i < numUsers; ++i)
+            std::cout << "\t" << "\"@" << profiles[i].getUsername() << "\"""" << std::endl;
 
         std::cout << std::endl;
-        for (int row = 0; row < MAX_USERS; row++)
-        {
-            for (int col = 0; col < MAX_USERS; col++)
-            {
-                if (following[row][col] == true)
+        for (int row = 0; row < MAX_USERS; ++row)
+            for (int col = 0; col < MAX_USERS; ++col)
+                if (following[row][col])
                     std::cout << "\t\"@" << profiles[row].getUsername() << "\"""" << " -> " << "\"@" << profiles[col].getUsername() << "\"""" << std::endl;
-            }
-        }
         std::cout << "}" << std::endl;
     }
 
-    // Add a new post
-    bool writePost(string usrn, string msg)
+    bool writePost(const string& usrn, const string& msg)
     {
-        if (numPosts < MAX_POSTS)
-        {
-            if (findID(usrn) != -1)
-            {
-                Post newMsg;
-                newMsg.username = usrn;
-                newMsg.message = msg;
+        if (numPosts == MAX_POSTS || findID(usrn) == -1)
+            return false;
 
-                posts[numPosts] = newMsg;
-                numPosts++;
-                return true;
-            }
-        }
+        Post newMsg;
+        newMsg.username = usrn;
+        newMsg.message = msg;
 
-        return false;
+        posts[numPosts] = newMsg;
+        ++numPosts;
+
+        return true;
     }
 
-    // Print user's "timeline"
-    void printTimeline(string usrn) 
+    void printTimeline(const string& usrn) const
     {
-        for (int i = numPosts; i >= 0; i--)
+        for (int i = numPosts; i >= 0; --i)
         {
-            if (posts[i].username == usrn)
-                std::cout << profiles[findID(usrn)].getFullName() << ": " << posts[i].message << std::endl;
+            int id = findID(usrn), followId = findID(posts[i].username);
 
-            else if (following[findID(usrn)][findID(posts[i].username)] == true)
-                std::cout << profiles[findID(posts[i].username)].getFullName() << ": " << posts[i].message << std::endl;
+            if (posts[i].username == usrn)
+                std::cout << profiles[id].getFullName() << ": " << posts[i].message << std::endl;
+
+            else if (following[id][followId])
+                std::cout << profiles[followId].getFullName() << ": " << posts[i].message << std::endl;
         }
     }
 };
